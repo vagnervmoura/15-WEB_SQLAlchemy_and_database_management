593be9d0255afe0a8ff105bfea8541680a0d6480
Vagner Vignoto Moura <eu@vagnermoura.com>
2024-02-23 17:55:56 +0000

    Adding Procfile

--- app.py
+++ app.py
@@ -419,4 +419,7 @@ def update_history(transaction, value):
 
 # upload website on-line
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(system.getenv('PORT'), '5000')
+    app.run(host='0.0.0.0', port = port)
+    
+    # app.run(debug=True)
